18264f2bd05e2f4179cb0da0aabcd3d0cac3076d
Brian Schott <brian@bronxsoftware.org>
2018-08-15 13:03:57 +0000

    First

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,27 @@
+#There are 4 mistakes in here!
+
+require './config/environment'
+require './app/models/sample_model'
+
+class ApplicationController < Sinatra::Base
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get '/memes' do
+
+    return erb :blah
+  end
+  
+  get '/results' do
+    @wordToCheck = params[:breakfastitem]
+    if palindromeChecker(@wordToCheck) == true
+      @result = "That's a palindrome!"
+    else
+      @result = "Not a palindrome! :("
+    end
+    return erb :index
+  end
+  
+end
---
+++ app/models/sample_model.rb
@@ -0,0 +1,10 @@
+# There's one error in here!
+def palindromeChecker(word)
+    return false
+    
+    if word == word.reverse
+       return true
+    else
+       return false
+    end
+end
